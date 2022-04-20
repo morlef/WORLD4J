189627c70080666d62e701eb23d3037b88fb0fa1
morlef <luna.runtime@gmail.com>
2022-04-20 05:48:26 +0000

    Update

--- src/main/java/world/api/wave/WaveIO.java
+++ src/main/java/world/api/wave/WaveIO.java
@@ -1,9 +1,12 @@
 package world.api.wave;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -13,6 +16,22 @@ import world.api.AudioInterface;
 public record WaveIO(File file) implements AudioInterface {
     @Override
     public void write(double[] x, int fs, int nbit) {
+            byte[] buf = new byte[x.length * fs];
+
+            for (int i = 0;i < x.length;i++) {
+                ByteBuffer.wrap(buf, i * fs, fs).putDouble(x[i]);
+            }
+
+            try {
+                AudioInputStream stream = AudioSystem.getAudioInputStream(new ByteArrayInputStream(buf));
+                AudioSystem.write(stream, AudioFileFormat.Type.WAVE, new FileOutputStream(file));
+            } catch (UnsupportedAudioFileException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
     }
 
     @Override
