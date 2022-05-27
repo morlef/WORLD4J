7c20da3a419cd7cd640001443676e7ae9fa26844
morlef <luna.runtime@gmail.com>
2022-05-27 01:44:07 +0000

    Update Debug.java

--- src/test/java/world/Debug.java
+++ src/test/java/world/Debug.java
@@ -1,7 +1,20 @@
 package world;
 
+import java.io.File;
+
 public class Debug {
     public static void main(String[] args) {
+        new Debug(new File(args[0]));
+    }
 
+    public Debug(File file) {
+
+    }
+
+    public void DisplayInformation(int fs, int nbit, int x_length) {
+        World.getLogger().info("File information\n");
+        World.getLogger().info("Sampling : %d Hz %d Bit\n", fs, nbit);
+        World.getLogger().info("Length %d [sample]\n", x_length);
+        World.getLogger().info("Length %f [sec]\n", (double) (x_length) / fs);
     }
 }
